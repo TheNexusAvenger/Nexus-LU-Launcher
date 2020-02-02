422f736b1c974ddc43f57790d03214468947c11c
TheNexusAvenger <zpcook@googlemail.com>
2020-02-02 15:56:49 -0500

    Removed old code.

--- NLUL.CLI/Program.cs
+++ NLUL.CLI/Program.cs
@@ -3,13 +3,12 @@
  *
  * Runs the program.
  */
+
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading;
 using NLUL.Action;
 using NLUL.Core;
-using NLUL.Core.Server;
 
 namespace NLUL
 {
@@ -17,27 +16,6 @@ namespace NLUL
     {
         static void Main(string[] args)
         {
-            /*
-            var systemInfo = new SystemInfo(args[0],args[1]);
-            var serverCreator = new ServerCreator(systemInfo);
-            var emulator = serverCreator.CreateServer("UchuDev", ServerType.Uchu);
-
-            foreach (var prerequisite in emulator.GetPrerequisites())
-            {
-                Console.WriteLine("Checking " + prerequisite.GetName());
-                if (!prerequisite.IsMet())
-                {
-                    Console.WriteLine("Failed: " + prerequisite.GetName());
-                    throw new Exception("Failed to install");
-                }
-            }
-            
-            emulator.Install();
-            emulator.Start();
-            Thread.Sleep(30000);
-            emulator.Stop();
-            */
-            
             // Print the usage information if the arguments are invalid.
             if (args.Length < 1)
             {
