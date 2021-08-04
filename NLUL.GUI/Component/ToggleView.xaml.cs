6641c0d343aa32e416aeb6ede870a64231dd2f0c
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-08-03 20:59:07 -0400

    Remove hosts view. Make toggle buttons common. Add settings button.

--- NLUL.GUI/Component/ToggleView.xaml.cs
+++ NLUL.GUI/Component/ToggleView.xaml.cs
@@ -4,9 +4,10 @@
  * Toggles between the play view and host view.
  */
 
+using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using NLUL.GUI.Component.Host;
+using NLUL.GUI.Component.Base;
 using NLUL.GUI.Component.Patches;
 using NLUL.GUI.Component.Play;
 
@@ -15,14 +16,13 @@ namespace NLUL.GUI.Component
     public enum ActiveView
     {
         Play,
-        Host,
         Patches,
+        Settings,
     }
     
     public class ToggleView : Canvas
     {
         private PlayView playView;
-        private HostView hostView;
         private PatchesView patchesView;
         
         /*
@@ -33,11 +33,32 @@ namespace NLUL.GUI.Component
             // Load the XAML.
             AvaloniaXamlLoader.Load(this);
             this.playView = this.Get<PlayView>("PlayView");
-            this.hostView = this.Get<HostView>("HostView");
             this.patchesView = this.Get<PatchesView>("PatchesView");
 
             // Set the active view.
             this.SetView(ActiveView.Play);
+            
+            // Connect the events.
+            this.Get<ImageTextButton>("GitHubButton").ButtonPressed += (sender, args) =>
+            {
+                // Open the repository in a browser.
+                var webProcess = new Process(); 
+                webProcess.StartInfo.FileName = "https://github.com/TheNexusAvenger/Nexus-Lego-Universe-Launcher";
+                webProcess.StartInfo.UseShellExecute = true;
+                webProcess.Start(); 
+            };
+            this.Get<ImageTextButton>("PlayButton").ButtonPressed += (sender, args) =>
+            {
+                this.SetView(ActiveView.Play);
+            };
+            this.Get<ImageTextButton>("PatchesButton").ButtonPressed += (sender, args) =>
+            {
+                this.SetView(ActiveView.Patches);
+            };
+            this.Get<ImageTextButton>("SettingsButton").ButtonPressed += (sender, args) =>
+            {
+                this.SetView(ActiveView.Settings);
+            };
         }
         
         /*
@@ -47,7 +68,6 @@ namespace NLUL.GUI.Component
         {
             // Update the visibility.
             this.playView.IsVisible = (view == ActiveView.Play);
-            this.hostView.IsVisible = (view == ActiveView.Host);
             this.patchesView.IsVisible = (view == ActiveView.Patches);
         }
     }
