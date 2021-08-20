da7e9987a67f76878c4482b495cf8560e5795816
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-08-20 00:19:32 -0400

    Disable patching if client isn't downloaded.

--- NLUL.GUI/Component/Patches/PatchesView.xaml.cs
+++ NLUL.GUI/Component/Patches/PatchesView.xaml.cs
@@ -1,13 +1,25 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using NLUL.Core;
 using NLUL.GUI.State;
 
 namespace NLUL.GUI.Component.Patches
 {
     public class PatchesView : StackPanel
     {
+        /// <summary>
+        /// Message for the client not being download.
+        /// </summary>
+        private readonly TextBlock notDownloadedMessage;
+        
+        /// <summary>
+        /// Scroll view of the patches.
+        /// </summary>
+        private readonly ScrollViewer patchesScroll;
+        
         /// <summary>
         /// Display list of the patches.
         /// </summary>
@@ -25,13 +37,32 @@ namespace NLUL.GUI.Component.Patches
         {
             // Load the XAML.
             AvaloniaXamlLoader.Load(this);
+            this.notDownloadedMessage = this.Get<TextBlock>("NotDownloadedMessage");
+            this.patchesScroll = this.Get<ScrollViewer>("PatchesScroll");
             this.patchesList = this.Get<StackPanel>("PatchesList");
             
             // Add the patch frames.
             this.ReloadPatches();
             Client.ClientSourceChanged += this.ReloadPatches;
+            
+            // Connect updating the patches visibility.
+            Client.StateChanged += this.UpdateVisibility;
+            this.UpdateVisibility();;
         }
 
+        /// <summary>
+        /// Updates the visibility of the patches.
+        /// </summary>
+        private void UpdateVisibility()
+        {
+            var canPatch = File.Exists(Path.Combine(SystemInfo.GetDefault().ClientLocation, "legouniverse.exe"));
+            this.notDownloadedMessage.IsVisible = !canPatch;
+            this.patchesScroll.IsVisible = canPatch;
+        }
+        
+        /// <summary>
+        /// Reloads the patches.
+        /// </summary>
         private void ReloadPatches()
         {
             // Clear the patches.
