ee889a40c648cb705ddfdbced5c9aabceebb6dbe
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2024-01-30 08:49:44 -0500

    Migrate initial toggle view.

---
+++ Nexus.LU.Launcher.Gui/Component/Base/ImageTextButton.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Reflection;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace Nexus.LU.Launcher.Gui.Component.Base;
+
+public class ImageTextButton : Panel
+{
+    /// <summary>
+    /// Event for the button being pressed.
+    /// </summary>
+    public static readonly RoutedEvent<RoutedEventArgs> ButtonPressedEvent = RoutedEvent.Register<InputElement, RoutedEventArgs>(nameof(ButtonPressed), RoutingStrategies.Direct);
+    public event EventHandler<RoutedEventArgs> ButtonPressed
+    {
+        add => AddHandler(ButtonPressedEvent, value);
+        remove => RemoveHandler(ButtonPressedEvent, value);
+    }
+    
+    /// <summary>
+    /// Image of the button.
+    /// </summary>
+    public string Image
+    {
+        get => GetValue(ImageProperty);
+        set => SetValue(ImageProperty,value);
+    }
+    public static readonly StyledProperty<string> ImageProperty = AvaloniaProperty.Register<Window, string>(nameof(Image), "");
+    
+    /// <summary>
+    /// Text of the button.
+    /// </summary>
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<Window, string>(nameof(Text), "");
+    
+    /// <summary>
+    /// Size of the font of the button.
+    /// </summary>
+    public double FontSize
+    {
+        get => GetValue(FontSizeProperty);
+        set => SetValue(FontSizeProperty,value);
+    }
+    public static readonly StyledProperty<double> FontSizeProperty = AvaloniaProperty.Register<Window, double>(nameof(FontSize), 14);
+    
+    /// <summary>
+    /// Image of the button.
+    /// </summary>
+    private readonly Image buttonImage;
+    
+    /// <summary>
+    /// Text of the button.
+    /// </summary>
+    private readonly TextBlock buttonText;
+    
+    /// <summary>
+    /// Whether the button is hovering.
+    /// </summary>
+    private bool hovering;
+    
+    /// <summary>
+    /// Whether the button is pressing.
+    /// </summary>
+    private bool pressing;
+    
+    /// <summary>
+    /// Creates the image button.
+    /// </summary>
+    public ImageTextButton()
+    {
+        // Load the XAML.
+        AvaloniaXamlLoader.Load(this);
+        this.buttonImage = this.Find<Image>("ButtonImage")!;
+        this.buttonText = this.Find<TextBlock>("ButtonText")!;
+
+        // Connect the events.
+        this.PropertyChanged += (sender, args) =>
+        {
+            this.UpdateButton();
+        };
+        this.PointerEntered += (sender, args) =>
+        {
+            this.hovering = true;
+            this.UpdateButton();
+        };
+        this.PointerExited += (sender, args) =>
+        {
+            this.hovering = false;
+            this.pressing = false;
+            this.UpdateButton();
+        };
+        this.PointerPressed += (sender, args) =>
+        {
+            this.pressing = true;
+            this.UpdateButton();
+            args.Handled = true;
+        };
+        this.PointerReleased += (sender, args) =>
+        {
+            if (!this.pressing) return;
+            this.pressing = false;
+            this.UpdateButton();
+            RaiseEvent(new RoutedEventArgs(ButtonPressedEvent));
+        };
+    }
+    
+    /// <summary>
+    /// Updates the button.
+    /// </summary>
+    private void UpdateButton()
+    {
+        // Update the image and color.
+        if (this.Image != "")
+        {
+            this.buttonImage.Source = new Bitmap(AssetLoader.Open(new Uri($"avares://{Assembly.GetEntryAssembly()?.GetName().Name}{this.Image}")));
+        }
+        this.buttonImage.Width = this.Height;
+        this.buttonImage.Height = this.Height;
+        this.buttonText.Text = this.Text;
+        this.buttonText.FontSize = this.FontSize;
+
+        // Update the text color.
+        if (this.pressing || this.hovering)
+        {
+            this.buttonText.Foreground = new SolidColorBrush(new Color(255, 255, 255, 255));
+        }
+        else
+        {
+            this.buttonText.Foreground = new SolidColorBrush(new Color(255, 127, 127, 127));
+        }
+    }
+}
---
+++ Nexus.LU.Launcher.Gui/Component/ToggleView.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Nexus.LU.Launcher.Gui.Component.Base;
+using Nexus.LU.Launcher.State.Client;
+using Nexus.LU.Launcher.State.Enum;
+
+namespace Nexus.LU.Launcher.Gui.Component;
+
+public enum ActiveView
+{
+    Play,
+    Patches,
+    Settings,
+}
+
+public class ToggleView : Canvas
+{
+    /// <summary>
+    /// View of the server list.
+    /// </summary>
+    // TODO: private readonly PlayView playView;
+    
+    /// <summary>
+    /// View of the patches.
+    /// </summary>
+    // TODO: private readonly PatchesView patchesView;
+    
+    /// <summary>
+    /// View of the settings.
+    /// </summary>
+    // TODO: private readonly SettingsView settingsView;
+    
+    /// <summary>
+    /// Creates a toggle view panel.
+    /// </summary>
+    public ToggleView()
+    {
+        // Load the XAML.
+        AvaloniaXamlLoader.Load(this);
+        // TODO: this.playView = this.Get<PlayView>("PlayView");
+        // TODO: this.patchesView = this.Get<PatchesView>("PatchesView");
+        // TODO: this.settingsView = this.Get<SettingsView>("SettingsView");
+        var playButton = this.Get<ImageTextButton>("PlayButton");
+        var patchesButton = this.Get<ImageTextButton>("PatchesButton");
+        var settingsButton = this.Get<ImageTextButton>("SettingsButton");
+
+        // Set the active view.
+        this.SetView(ActiveView.Play);
+        
+        // Connect the events.
+        var clientState = ClientState.Get();
+        this.Get<ImageTextButton>("GitHubButton").ButtonPressed += (sender, args) =>
+        {
+            // Open the repository in a browser.
+            var webProcess = new Process(); 
+            webProcess.StartInfo.FileName = "https://github.com/TheNexusAvenger/Nexus-Lego-Universe-Launcher";
+            webProcess.StartInfo.UseShellExecute = true;
+            webProcess.Start(); 
+        };
+        playButton.ButtonPressed += (sender, args) =>
+        {
+            this.SetView(ActiveView.Play);
+        };
+        patchesButton.ButtonPressed += (sender, args) =>
+        {
+            this.SetView(ActiveView.Patches);
+        };
+        settingsButton.ButtonPressed += (sender, args) =>
+        {
+            this.SetView(ActiveView.Settings);
+        };
+        clientState.LauncherStateChanged += (state) =>
+        {
+            playButton.IsVisible = (state != LauncherState.Launched);
+            patchesButton.IsVisible = (state != LauncherState.Launched);
+            settingsButton.IsVisible = (state != LauncherState.Launched);
+        };
+    }
+    
+    /// <summary>
+    /// Sets the view to use.
+    /// </summary>
+    /// <param name="view">View to use.</param>
+    private void SetView(ActiveView view)
+    {
+        // Update the visibility.
+        // TODO: this.playView.IsVisible = (view == ActiveView.Play);
+        // TODO: this.patchesView.IsVisible = (view == ActiveView.Patches);
+        // TODO: this.settingsView.IsVisible = (view == ActiveView.Settings);
+    }
+}
