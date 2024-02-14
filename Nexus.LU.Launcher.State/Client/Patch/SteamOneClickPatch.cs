5a87be370e17c66fbb46d830ee0fefdd6c98ece7
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2024-02-14 16:02:36 -0500

    Ignore Steam restart in Flatpak if Steam is not running.

--- Nexus.LU.Launcher.State/Client/Patch/SteamOneClickPatch.cs
+++ Nexus.LU.Launcher.State/Client/Patch/SteamOneClickPatch.cs
@@ -160,18 +160,27 @@ public class SteamOneClickPatch : IPreLaunchClientPatch
             {
                 // Store the last time the log had "Shutdown".
                 // It is assumed that if the line is different, then a new shutdown was performed by the user.
-                Logger.Warn("Unable to restart Steam from a Flatpak. A manual restart will be required.");
                 var bootstrapLogPath = Path.Combine(this.steamLocation!, "logs", "bootstrap_log.txt");
                 if (File.Exists(bootstrapLogPath))
                 {
                     var lastShutdownLine = "";
-                    foreach (var line in await File.ReadAllLinesAsync(bootstrapLogPath))
+                    var bootstrapLogLines = await File.ReadAllLinesAsync(bootstrapLogPath);
+                    if (bootstrapLogLines.Length >= 1 && bootstrapLogLines[^1].Contains("Shutdown"))
                     {
-                        if (!line.Contains("Shutdown")) continue;
-                        lastShutdownLine = line;
+                        Logger.Info("Steam is not active and does not need to be restarted.");
                     }
-                    this.systemInfo.SetPatchStore("SteamOneClick", "LastSteamShutdownLine", lastShutdownLine);
-                    this.systemInfo.SaveSettings();
+                    else
+                    {
+                        Logger.Warn("Unable to restart Steam from a Flatpak. A manual restart will be required.");
+                        foreach (var line in bootstrapLogLines)
+                        {
+                            if (!line.Contains("Shutdown")) continue;
+                            lastShutdownLine = line;
+                        }
+
+                        this.systemInfo.SetPatchStore("SteamOneClick", "LastSteamShutdownLine", lastShutdownLine);
+                        this.systemInfo.SaveSettings();
+                    }
                 }
             }
         }
